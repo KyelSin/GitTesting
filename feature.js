dfaa1c9908cd17d5087e5bdce454ee1cb8d70620
KyelSin <kyelsinshunlet@gmail.com>
2025-01-10 13:53:11 +0630

    feature.js in feature-1 branch

---
+++ feature.js
@@ -0,0 +1,17 @@
+let intro = "Hello, this is greeting from feature.js in feature-1 branch.";
+console.log(intro);
+
+const testingNum = [100, 200, 150, 80, 50, 40, 45, 180, 230];
+const testingFruits = ["apple", "mango", "papaya", "banana", "burberry", "strawberry"];
+
+for (let i = 0; i < testingFruits.length; i++) {
+    let eachFruit = testingFruits[i];
+    console.log(eachFruit);
+}
+console.log(`============`);
+
+for (let i = 0; i < testingNum.length; i++) {
+    let eachNum = testingNum[i];
+    console.log(eachNum);
+}
+console.log(`============`);
